12ccf2c5d623ed614a426c32cf9a537d30c2eaa4
antho2222 <anthonyvazquez@ginebro.cat>
2024-06-07 16:06:14 +0200

    Primer commit 16:06

---
+++ src/Recus/Persona.java
@@ -0,0 +1,13 @@
+package Recus;
+
+public class Persona {
+    private boolean tePermisDeConduir;
+    private String nom;
+    private int edat;
+
+    public Persona(boolean _tePermisDeConduir, String _nom, int _edat) {
+        this.tePermisDeConduir = _tePermisDeConduir;
+        this.nom = _nom;
+        this.edat = _edat;
+    }
+}
